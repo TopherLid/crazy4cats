4fb81596c43f05257c805798caa405e6a50ff3fe
Christopher Paredes <topherpl98@gmail.com>
2023-06-15 21:49:33 -0400

    fix de vistas

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
     private
 
     def set_post
-        @comment = Comment.find(params[:id])
+        @post = Post.find(params[:id])
     end
 
     def post_params
